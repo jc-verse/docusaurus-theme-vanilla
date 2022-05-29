cc76b0b30670cda6925d58088566d6f7d6ead327
Joshua Chen <sidachen2003@gmail.com>
2022-05-29 23:07:35 +0800

    fix deploy failure

--- src/components/Zoom/index.tsx
+++ src/components/Zoom/index.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from "react";
-import { useColorMode } from "@docusaurus/theme-common";
 import BasicZoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 
@@ -15,14 +14,8 @@ export default function Zoom({
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  const { colorMode } = useColorMode();
   return (
-    <BasicZoom
-      overlayBgColorEnd={
-        colorMode === "dark"
-          ? "rgba(0, 0, 0, 0.95)"
-          : "rgba(255, 255, 255, 0.95)"
-      }>
+    <BasicZoom overlayBgColorEnd="rgba(255, 255, 255, 0.95)">
       {children}
     </BasicZoom>
   );
